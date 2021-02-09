ee59e9cd12e77961bb61b3054d6710000159f6ec
Jack Miller <jacklmiller97@gmail.com>
2021-02-09 01:53:21 -0500

    Added styling and fixed scaling/presentation issues

--- src/js/DogeBlock.js
+++ src/js/DogeBlock.js
@@ -1,14 +1,22 @@
 export class DogeBlock {
 	constructor(block) {
-		this.block = block;
+		this.block    = block;
+		this.duration = 15000
 	}
 
 	generateElement(container) {
-		const block  = this.block;
-		const height = block.size / 100;
-		const posY = Math.random()*container.offsetHeight;
+		const block = this.block;
+
+		let height = 80*(this.block.size/100000);
+		height = height <= 100 ? height : 100;
+		height = height >= 10 ? height : 10;
+
+		const posY = Math.random()*(100 - height);
+
 		const html = `
-			<div id="${block.hash}" class="doge-block" style="width:${height}px; height:${height}px; position: absolute; top:${posY}; right:-10px;"> </div>
+			<div id="${block.hash}" class="doge-block" style="width:${height}vh; height:${height}vh; position: absolute; top:${posY}vh; right:-10px; animation-duration:${this.duration/1000}s;">
+				<div>${block.size/1000}KB</div>
+			</div>
 		`;
 		const nodeElement = new DOMParser().parseFromString(html, 'text/html');
 
@@ -18,5 +26,9 @@ export class DogeBlock {
 	addToDOM(container) {
 		const element = this.generateElement(container);
 		container.appendChild(element);
+
+		setTimeout(() => {
+			element.remove();
+		}, this.duration);
 	}
 }
